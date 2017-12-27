e598d8667448e0aa97cb645625bc8a2b86099439
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-27 15:44:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = 
+    @user =
     if User.find_by(username: params["username"], password: params["password"])
     @user = User.find_by(username: params["username"], password: params["password"])
     session[:id] = @user.id
